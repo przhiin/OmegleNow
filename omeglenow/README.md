34a45a27d555326c344c0cb7116e48f54ae0353e
przhiin <preshinraj7@gmail.com>
2025-04-29 16:31:51 +0530

    Delete omeglenow directory

--- omeglenow/README.md
+++
@@ -1,1 +0,0 @@
-# Preshin
--- omeglenow/chat/routing.py
+++
@@ -1,7 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/video/(?P<room_name>\w{8}-\w{4}-\w{4}-\w{4}-\w{12})/$', consumers.VideoChatConsumer.as_asgi()),
-]
